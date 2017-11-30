6f23a2d3a427512feab60e601bbf695c75f1458f
Brian <bjudaprawira@horizon.csueastbay.edu>
2017-11-29 22:51:15 -0800

    proj2 att4

--- controllers/database.js
+++ controllers/database.js
@@ -1,68 +1,16 @@
 var mongodb = require('mongodb');
 var mongoDBURI = process.env.MONGODB_URI || 'mongodb://newUser:brave123@ds259105.mlab.com:59105/heroku_6rdr5zwb';
 var bodyParser = require('body-parser');
-var path = require ('path');
+var multer = require('multer');
+var upload = multer();
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
 
-module.exports.getAllOrders =  function (request, response) {
-
+module.exports.storeData = function(req, response){
     mongodb.MongoClient.connect(mongoDBURI, function(err, db) {
         if(err) throw err;
 
-        //get collection of orders
-        var Orders = db.collection('Orders');
-
-
-        //FIRST showing you one way of making request for ALL orders and cycle through with a forEach loop on returned Cursor
-        //   this request and loop  is to display content in the  console log
-        var c = Orders.find({});
-
-        c.forEach(
-            function(myDoc) {
-                console.log( "name: " + myDoc.name );  //just  loging the output to the console
-            }
-        );
-
-
-        //SECOND -show another way to make request for ALL Orders  and simply collect the  documents as an
-        //   array called docs that you  forward to the  getAllOrders.ejs view for use there
-        Orders.find().toArray(function (err, docs) {
-            if(err) throw err;
-
-            response.render('getAllOrders', {results: docs});
-
-        });
-
-
-        //Showing in comments here some alternative read (find) requests
-        //this gets Orders where frequency>=10 and sorts by name
-        // Orders.find({ "frequency": { "$gte": 10 } }).sort({ name: 1 }).toArray(function (err, docs) {
-        // this sorts all the Orders by name
-        //  Orders.find().sort({ name: 1 }).toArray(fu namenction (err, docs) {
-
-
-        //close connection when your app is terminating.
-        db.close(function (err) {
-            if(err) throw err;
-        });
-    });//end of connect
-};//end function
-
-module.exports.storeData = function(request, response){
-    mongodb.MongoClient.connect(mongoDBURI, function(err, db) {
-        if(err) throw err;
-
-        /**************************************************************************
-         * IMPORTANT:  this is how you generate  a random number for  3IDs that
-         * you will need for the collections cusomerID, billinID and   shippingID
-         *    WHY?  the retrieve _id  info after and  insert (see below)  does not seem
-         *     to function properly on Heroku
-         *    so to know the ID we simply generate it in code  rather than
-         *     autogenerate it for the documents we newly insert into the CUSOTMERS, BILLING, SHIPPING
-         *      for ORDERS we allow the system to autogenerate its  _id
-         */
         var customerID = Math.floor((Math.random() * 1000000000000) + 1);
         var billingID = Math.floor((Math.random() * 1000000000000) + 1);
         var shippingID = Math.floor((Math.random() * 1000000000000) + 1);
@@ -72,31 +20,31 @@ module.exports.storeData = function(request, response){
 
         var customerdata = {
             _id: customerID,
-            FIRSTNAME: req.body.user.FIRSTNAME,
-            LASTNAME: null,
-            STREET: req.body.user.STREET,
-            CITY: req.body.user.CITY,
-            STATE: req.body.user.STATE,
-            ZIP: req.body.user.ZIP,
-            PHONE: null
+            FIRSTNAME: req.body.user.firstName,
+            LASTNAME: req.body.user.lastName,
+            STREET: req.body.user.street,
+            CITY: req.body.user.city,
+            STATE: req.body.user.state,
+            ZIP: req.body.user.zip,
+            PHONE: req.body.user.phone
         };
 
         var billingdata = {
             _id: billingID,
             CUSTOMER_ID: customerID,
-            BILLING_CITY: shipment_info['city'],
-            BILLING_STATE: shipment_info['state'],
-            BILLING_STREET: shipment_infos['add1'] + ' ' + shipment_info['add2'],
-            BILLING_ZIP: shipment_info['zipcode']
+            BILLING_CITY: req.body.user.billCity,
+            BILLING_STATE: req.body.user.billState,
+            BILLING_STREET: req.body.user.billStreet,
+            BILLING_ZIP: req.body.user.billZip
         };
 
         var shippingdata = {
             _id: shippingID,
             CUSTOMER_ID: customerID,
-            SHIPPING_STREET: shipment_info['add1'] + ' ' + shipment_info['add2'],
-            SHIPPING_CITY: shipment_info['city'],
-            SHIPPING_STATE: shipment_info['state'],
-            SHIPPING_ZIP: shipment_info['zipcode']
+            SHIPPING_STREET: req.body.user.street,
+            SHIPPING_CITY: req.body.user.city,
+            SHIPPING_STATE: req.body.user.state,
+            SHIPPING_ZIP: req.body.user.zip
         };
 
         var orderdata = {
@@ -128,33 +76,6 @@ module.exports.storeData = function(request, response){
             if (err) throw err;
             console.log("1 document inserted");
         });
-
-        //FIRST showing you one way of making request for ALL orders and cycle through with a forEach loop on returned Cursor
-        //   this request and loop  is to display content in the  console log
-        var c = Orders.find({});
-
-        c.forEach(
-            function(myDoc) {
-                console.log( "name: " + myDoc.name );  //just  loging the output to the console
-            }
-        );
-
-
-        //SECOND -show another way to make request for ALL Orders  and simply collect the  documents as an
-        //   array called docs that you  forward to the  getAllOrders.ejs view for use there
-        Orders.find().toArray(function (err, docs) {
-            if(err) throw err;
-
-            response.render('getAllOrders', {results: docs});
-
-        });
-
-
-        //Showing in comments here some alternative read (find) requests
-        //this gets Orders where frequency>=10 and sorts by name
-        // Orders.find({ "frequency": { "$gte": 10 } }).sort({ name: 1 }).toArray(function (err, docs) {
-        // this sorts all the Orders by name
-        //  Orders.find().sort({ name: 1 }).toArray(fu namenction (err, docs) {
 
 
         //close connection when your app is terminating.
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var mongodb = require('mongodb')
-var multer = multer();
+var multer = require('multer');
+var upload = multer();
 var path = require ('path');
+var bodyParser = require('body-parser');
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(upload.array());
@@ -12,39 +14,7 @@ var mongoDBURI = process.env.MONGODB_URI || 'mongodb://newUser:brave123@ds259105
 //LOAD the various controllers
 var controllerMongoCollection = require('../controllers/database'); //load controller code dealing with database mongodb and Orders collection
 
-router.get('/mongodb', function (request, response) {
-
-    mongodb.MongoClient.connect(mongoDBURI, function(err, db) {
-        if(err) throw err;
-
-        //get collection of orders
-        var Orders = db.collection('Orders');
-
-        //get all Orders
-        Orders.find({ }).sort({ name: 1 }).toArray(function (err, docs) {
-
-            if(err) throw err;
-
-            response.render('/getAllOrders', {results: docs});
-
-        });
-
-        //close connection when your app is terminating.
-        db.close(function (err) {
-            if(err) throw err;
-        });
-
-    });//end of connect
-
-});//end router.get
-
-router.get('/getAllOrders', controllerMongoCollection.getAllOrders);
-
 router.post('/storeData', controllerMongoCollection.storeData);
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
 module.exports = router;
